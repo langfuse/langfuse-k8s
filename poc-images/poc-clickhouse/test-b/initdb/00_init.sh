93dfc62dc865136dbe3c0904ef9f7792977a41d5
steffen911 <steffen@langfuse.com>
2026-07-22 15:03:38 +0200

    chore: cleanup

--- poc-images/poc-clickhouse/test-b/initdb/00_init.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-clickhouse-client --user default --password "$CLICKHOUSE_ADMIN_PASSWORD" --query "CREATE TABLE initdb_marker (x UInt8) ENGINE = TinyLog"
-clickhouse-client --user default --password "$CLICKHOUSE_ADMIN_PASSWORD" --query "INSERT INTO initdb_marker VALUES (7)"
